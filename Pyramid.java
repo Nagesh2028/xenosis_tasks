79774292f8d8889730ed35388189b9496b762b41
Nagesh2028 <sainageshadabala@gmail.com>
2024-07-21 10:41:06 +0530

    Add files via upload

---
+++ Pyramid.java
@@ -0,0 +1,12 @@
+package org.xenosis.assigment2;
+
+public class Pyramid {
+	    public static void main(String[] args) {
+	        for(int z=6;z>=1;z=z-1){
+	            for(int j=1;j<=z;j=j+1)
+	                System.out.print(j+" ");
+	           System.out.print("\n"); 
+	        }
+	    }
+
+	}
